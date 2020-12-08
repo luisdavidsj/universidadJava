5c7a139fb85ae398ee252968b962b820aab47721
luisd <luisd@SPIDERWOLF-PC>
2020-12-08 03:15:50 -0600

    Terminando casi la sección 2.

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,50 +1,58 @@
 //Mi clase en Java
 public class HolaMundo {
     public static void main(String args[]){
-        /********************************
-        ***********Sección 1*************
-        ********************************/
+        /*
+        **Sección 1
+        **Fundamentos Java: Instalación Java 15 y Apache NetBeans
+        */
         System.out.println("Hola mundo");
         
-        //Definimos la variable
+        /*
+        **Sección 2
+        **Fundamentos Java: Variables en Java
+        */
+        
+        //Definimos la variable tipo: int
         int miVariableEntera = 10;
         System.out.println(miVariableEntera);
-        
         //Modificamos el valor de la variable
         miVariableEntera = 5;
         System.out.println(miVariableEntera);
         
+        //Definimos la variable tipo: String
         String miVariableCadena = "Hola";
         System.out.println(miVariableCadena);
-        
+        //Modificamos el valor de la variable
         miVariableCadena = "Adiós";
         System.out.println(miVariableCadena);
         
-        //test, BORRAR
-        String myString = "qwerty123456";
-        if (myString.matches("[A-Za-z0-9]+")){
-            System.out.println("alfanumerico OK");
-        }
+        //var - Inferencia de tipos en Java
+        var miVariableEntera2 = 15;
+        System.out.println(miVariableEntera2);
+        var miVariableCadena2 = "Nueva Cadena";
+        System.out.println("miVariableCadena2 = " + miVariableCadena2);
+        
+        //Valores permitidos en el nombre de variables
+        var miVariable = 1;//La regla más recomendada
+        var _miVariable = 2;
+        var $miVariable = 3;
+        //var áVariable = 10; No se recomienda utilizar
+        //var #miVariable = 2; No se permite utilizar caracteres especiales
+        
+        //Concatenacion
+        var usuario = "Juan";
+        var titulo = "Ingeniero";
 
-        //otro test
-        int i = 10;
-
-        if (i == 10) {
-            System.out.println("Nombre medicamento verificado");
-            if (i < 15) {
-                System.out.println("Tipo medicamento verificado");
-                if (i < 14){
-                    System.out.println("Cantidad del producto verificado");
-                    if (i < 13){
-                        System.out.println("Distrivuidor OK");
-                        if (i < 12){
-                            System.out.println("Sucursal OK");
-                        }
-                    }
-                }
-            }
-        } else {
-            System.out.println("MENSAJE DE ERROR");
-        }
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
+        
+        var i = 3;
+        var j = 4;
+        
+        System.out.println(i + j);//Se realiza la suma de números
+        System.out.println(i + j + usuario);//Evaluacion de izq a der, se realiza suma
+        System.out.println(usuario + i + j);//Contexto cadena, todo es una cadena
+        System.out.println(usuario + (i + j));//Uso de parentesis modifican la prioridad en la evaluación
+        
     }
 }
