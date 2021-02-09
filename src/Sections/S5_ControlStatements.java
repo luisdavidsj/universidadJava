e6b76f6fe50f4dd93767cf0d4b626a648bc3e27e
luisd <luisd@SPIDERWOLF-PC>
2021-02-09 01:27:13 -0600

    Sección 5, lección 33. Sentencia de control if - else en Java.

---
+++ src/Sections/S5_ControlStatements.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////////////////
+//Sección 5: Fundamentos Java: Operadores en Java
+////////////////////////////////////////////////////////////////////////////////
+package Sections;
+
+public class S5_ControlStatements {
+
+    public static void main(String[] args) {
+        var condicion = true;
+
+        if (condicion) {
+            System.out.println("Condicion verdadera");
+        } else {
+            System.out.println("Condicion falsa");
+        }
+    }
+}
