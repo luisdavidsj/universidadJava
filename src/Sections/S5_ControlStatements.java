2fa4de84bec9b1b9626b8918d8a5f488d04c6d5d
luisd <luisd@SPIDERWOLF-PC>
2021-02-10 00:07:26 -0600

    Sección 5, lección 34. Ejercicio If - else en Java.

--- src/Sections/S5_ControlStatements.java
+++ src/Sections/S5_ControlStatements.java
@@ -6,12 +6,34 @@ package Sections;
 public class S5_ControlStatements {
 
     public static void main(String[] args) {
+        //33. Sentencia de control if - else en Java
         var condicion = true;
-
         if (condicion) {
             System.out.println("Condicion verdadera");
         } else {
             System.out.println("Condicion falsa");
         }
+        
+        //34. Ejercicio If - else en Java
+        var numero = 4;
+        var numeroTexto = "Numero desconocido";
+        
+        if (numero == 1) {
+            numeroTexto = "Numero Uno";
+        }
+        else if (numero == 2) {
+            numeroTexto = "Numero Dos";
+        }
+        else if (numero == 3) {
+            numeroTexto = "Numero Tres";
+        }
+        else if (numero == 4) {
+            numeroTexto = "Numero Cuatro";
+        }
+        else{
+            numeroTexto = "Numero no encontrado";
+        }
+        
+        System.out.println("numeroTexto = " + numeroTexto);
     }
 }
